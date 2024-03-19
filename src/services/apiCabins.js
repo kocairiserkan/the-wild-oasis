4505d39b6969124cf5a093401e4206238093c3bc
kocairiserkan <serkankocairi@outlook.com>
2024-03-19 09:01:03 +0000

    deleted items using useMutation hook

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,16 @@ export async function getCabins() {
 
     return data;
 }
+
+export async function deleteCabin(id){
+    
+    const { data, error } = await supabase.from('cabins').delete().eq('id', id);
+
+    if(error){
+        console.error(error);
+        throw new Error('Cabin could not be loaded');
+    }
+
+    return data;
+
+}
