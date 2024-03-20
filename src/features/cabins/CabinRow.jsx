94ddd1443e2a5e049b45b601f8d294dccf3ee418
kocairiserkan <serkankocairi@outlook.com>
2024-03-20 10:30:29 +0000

    react query custom hooks

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import {formatCurrency} from "../../utils/helpers";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteCabin } from "../../services/apiCabins";
-import toast from "react-hot-toast";
 import {useState} from "react";
+
 import CreateCabinForm from "./CreateCabinForm";
+import { useDeleteCabin } from "./useDeleteCabin";
+import {formatCurrency} from "../../utils/helpers";
+
 
 const TableRow = styled.div`
   display: grid;
@@ -47,21 +47,12 @@ const Discount = styled.div`
 
 function CabinRow({cabin}) {
   const [showForm, setShowForm] = useState(false);
+  const {isDeleting, deleteCabin} = useDeleteCabin();
 
   const {id: cabinId, name, maxCapacity, regularPrice, discount, image} = cabin;
 
-  const queryClient = useQueryClient();
-
-  const {isLoading: isDeleting, mutate} = useMutation({
-    mutationFn: deleteCabin,
-    onSuccess: () => {
-      toast.success('Cabin successfully deleted')
-      queryClient.invalidateQueries({
-        queryKey: ['cabins'],
-      });
-    },
-    onError: (err) => toast.error(err.message),
-  });
+  
+  
 
   return (
     <>
@@ -70,10 +61,10 @@ function CabinRow({cabin}) {
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
-      <Discount>{formatCurrency(discount)}</Discount>
+      {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
       <div>
         <button onClick={()=> setShowForm((show)=>!show)}>Edit</button>
-        <button onClick={() => mutate(cabinId)} disabled={isDeleting}>Delete</button>
+        <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>Delete</button>
       </div>
     </TableRow>
     {showForm && <CreateCabinForm cabinToEdit={cabin}/>}
