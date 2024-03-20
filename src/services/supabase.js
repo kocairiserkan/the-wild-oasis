02a3f7ca2cc43b515f535ee2d0291668d2aeb6cb
kocairiserkan <serkankocairi@outlook.com>
2024-03-20 03:46:54 +0000

    image uploading to the cabin

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://oapprqrnvlewrfbwpezu.supabase.co';
+export const supabaseUrl = 'https://oapprqrnvlewrfbwpezu.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hcHBycXJudmxld3JmYndwZXp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2NDEyMDEsImV4cCI6MjAyNjIxNzIwMX0.MsNO214pEJUd9Gr0IhfDsw_dlEl3tIScj3Wcf7Pc7_8";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
