e0354e1daa5217ecf730ad5056f3c2386891dee3
kocairiserkan <serkankocairi@outlook.com>
2024-03-22 16:40:04 +0000

    reusable context menu builded

--- src/features/cabins/CabinRow-v1.jsx
+++ src/features/cabins/CabinRow-v1.jsx
@@ -74,35 +74,39 @@ function CabinRow({cabin}) {
       <Price>{formatCurrency(regularPrice)}</Price>
       {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
       <div>
+        <button onClick={handleDuplicate} disabled={isCreating}>
+          <HiSquare2Stack/>
+        </button>
         
         <Modal>
-          <Menus.Menu>
-            <Menus.Toggle id={cabinId}/>
+          <Modal.Open opens='edit'>
+            <button>
+              <HiPencil/>
+            </button>
+          </Modal.Open>
+          <Modal.Window name='edit'>
+            <CreateCabinForm cabinToEdit={cabin}/>
+          </Modal.Window>
 
-            <Menus.List id={cabinId}>
+          <Modal.Open opens='delete'>
+            <button>
+              <HiTrash/>
+            </button>
+          </Modal.Open>
+          <Modal.Window name='delete'>
+            <ConfirmDelete resourceName='cabins' disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)}/>
+          </Modal.Window>
+        </Modal>
 
-              <Menus.Button icon={<HiSquare2Stack/>} onClick={handleDuplicate}>Duplicate</Menus.Button>
-              
-              <Modal.Open opens='edit'>
-                <Menus.Button icon={ <HiPencil/>}>Edit</Menus.Button>
-              </Modal.Open>
-              
-              <Modal.Open opens='delete'>
-                <Menus.Button icon={<HiTrash/>}>Delete</Menus.Button>
-              </Modal.Open>
+        <Menus.Menu>
+          <Menus.Toggle id={cabinId}/>
 
-            </Menus.List>
-         
-            <Modal.Window name='edit'>
-              <CreateCabinForm cabinToEdit={cabin}/>
-            </Modal.Window>
-
-          
-            <Modal.Window name='delete'>
-              <ConfirmDelete resourceName='cabins' disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)}/>
-            </Modal.Window>
+          <Menus.List id={cabinId}>
+            <Menus.Button icon={<HiSquare2Stack/>} onClick={handleDuplicate}>Duplicate</Menus.Button>
+            <Menus.Button icon={ <HiPencil/>}>Edit</Menus.Button>
+            <Menus.Button icon={<HiTrash/>}>Delete</Menus.Button>
+          </Menus.List>
         </Menus.Menu>
-      </Modal>
       </div>
     </Table.Row>
   )
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 
 const TableHeader = styled.header`
@@ -27,6 +28,7 @@ const {isLoading, cabins} = useCabins();
 if(isLoading) return <Spinner/>
 
   return (
+    <Menus>
     <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
       <Table.Header>
         <div></div>
@@ -38,7 +40,7 @@ if(isLoading) return <Spinner/>
       </Table.Header>
       <Table.Body data={cabins} render= {(cabin)=> (<CabinRow cabin={cabin} key={cabin.id}/>)}/>
     </Table>
-    
+    </Menus>
   )
 }
 
