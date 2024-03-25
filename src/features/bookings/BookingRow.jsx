cc0e25882b1b5a9fbcf73fccbfead32b84ec46d1
kocairiserkan <serkankocairi@outlook.com>
2024-03-25 13:32:29 +0000

    Checking in a booking

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -8,7 +8,7 @@ import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 
 import Menus from "../../ui/Menus";
-import { HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 
 const Cabin = styled.div`
@@ -53,7 +53,7 @@ function BookingRow({
   },
 }) {
   const navigate = useNavigate();
-  
+
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
@@ -89,9 +89,18 @@ function BookingRow({
       <Menus.Menu>
         <Menus.Toggle id={bookingId}/>
         <Menus.List id={bookingId}>
-          <Menus.Button icon={<HiEye/>} onClick={()=>navigate(`/bookings/${bookingId}`)}>
+
+          <Menus.Button icon={<HiEye/>} 
+                        onClick={()=>navigate(`/bookings/${bookingId}`)}>
             See details
           </Menus.Button>
+
+          {status === 'unconfirmed' && 
+          <Menus.Button icon={<HiArrowDownOnSquare/>} 
+                        onClick={()=>navigate(`/checkin/${bookingId}`)}>
+            Check in
+          </Menus.Button>}
+
         </Menus.List>
       </Menus.Menu>    
     </Table.Row>
