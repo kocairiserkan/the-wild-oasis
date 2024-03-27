971e316ec3f2381f96b099118fbec355ec042ee4
kocairiserkan <serkankocairi@outlook.com>
2024-03-27 09:16:23 +0000

    a small disabled added to button

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -11,18 +11,6 @@ import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 
 
-/*const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;*/
-
 const Img = styled.img`
   display: block;
   width: 6.4rem;
@@ -81,7 +69,12 @@ function CabinRow({cabin}) {
 
             <Menus.List id={cabinId}>
 
-              <Menus.Button icon={<HiSquare2Stack/>} onClick={handleDuplicate}>Duplicate</Menus.Button>
+              <Menus.Button icon={<HiSquare2Stack/>} 
+                            onClick={handleDuplicate}
+                            disabled={isCreating}
+              >
+                Duplicate
+              </Menus.Button>
               
               <Modal.Open opens='edit'>
                 <Menus.Button icon={ <HiPencil/>}>Edit</Menus.Button>
