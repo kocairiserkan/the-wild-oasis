1aa43484bfa8284db9c49db629c4178657759679
kocairiserkan <serkankocairi@outlook.com>
2024-03-27 13:04:03 +0000

    user log out created

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -23,3 +23,8 @@ export async function getCurrentUser() {
 
     return data?.user; //only want to return user itself
 }
+
+export async function logout() {
+    const {error} = await supabase.auth.signOut();
+    if(error) throw new Error(error.message);
+}
