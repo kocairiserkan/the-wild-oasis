48adec81152d3c6fde77afef2665f1d0ef2aad67
kocairiserkan <serkankocairi@outlook.com>
2024-03-29 17:04:29 +0000

    Stays displayed on home page

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -17,7 +18,7 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
   const {bookings, isLoading: isLoadingBookings} = useRecentBookings();
 
-  const {stays, confirmedStays, isLoading: isLoadingStays, numDays} = useRecentStays();
+  const {confirmedStays, isLoading: isLoadingStays, numDays} = useRecentStays();
 
   const {cabins, isLoading: isLoadingCabins} = useCabins();
 
@@ -29,7 +30,7 @@ function DashboardLayout() {
              confirmedStays={confirmedStays}
              numDays={numDays}
              cabinCount={cabins.length}/>
-      <div>Todays activity</div>
+      <TodayActivity/>
       <DurationChart confirmedStays={confirmedStays}/>
       <SalesChart bookings={bookings} numDays={numDays}/>
     </StyledDashboardLayout>
